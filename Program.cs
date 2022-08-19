5fa129247126a422bb657a3e8fe7664f43a44927
Boris <BorerKicker@yandex.ru>
2022-08-19 16:33:10 +0300

    added sort 10 items

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = {0, 2, 3, 2, 1, 5, 9, 1, 1};
+
+CountingSort(array);
+
+Console.WriteLine(string.Join(", ", array));
+
+void CountingSort(int[] inputArray)
+{
+    int[] counters = new int[10];
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        // counters[inputArray[i]]++;
+        int ourNumber = inputArray[i];
+        counters[ourNumber]++;
+    }
+
+    int index = 0;
+    for (int i = 0; i < counters.Length; i++)
+    {
+        for (int j = 0; j < counters[i]; j++)
+        {
+            inputArray[index] = i;
+            index++;   
+        }
+    }
+}
+
+
+
+
