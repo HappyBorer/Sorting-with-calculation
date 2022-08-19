aecce7469881ef77a43e811d4b1e38dd6b396a9a
Boris <BorerKicker@yandex.ru>
2022-08-19 16:51:22 +0300

    Added method Counting sort extended

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
-﻿int[] array = {0, 2, 3, 2, 1, 5, 9, 1, 1};
+﻿int[] array = {0, 2, 4, 10, 20, 5, 6, 1, 2};
 
-CountingSort(array);
-
+//CountingSort(array);
+CountingSortExtended(array);
 Console.WriteLine(string.Join(", ", array));
 
 void CountingSort(int[] inputArray)
@@ -15,6 +15,27 @@ void CountingSort(int[] inputArray)
     }
 
     int index = 0;
+    for (int i = 0; i < counters.Length; i++)
+    {
+        for (int j = 0; j < counters[i]; j++)
+        {
+            inputArray[index] = i;
+            index++;   
+        }
+    }
+} 
+
+void CountingSortExtended(int[] inputArray)
+{
+    int max = inputArray.Max();
+
+    int[] counters = new int[max + 1];
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        counters[inputArray[i]]++;
+    }
+
+     int index = 0;
     for (int i = 0; i < counters.Length; i++)
     {
         for (int j = 0; j < counters[i]; j++)
